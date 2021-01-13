45f0663529e8be6072837ba26a16e73a8252670c
qwer951123 <qiuhao951@gmail.com>
2021-01-13 17:37:20 +0800

    update bullet-bar

--- packages/ui-components/src/Chart/BulletBar.tsx
+++ packages/ui-components/src/Chart/BulletBar.tsx
@@ -1,14 +1,59 @@
-import React, { FC, createRef, useMemo, useLayoutEffect } from 'react';
+import React, { FC, createRef, useMemo, useLayoutEffect, useRef } from 'react';
+import styled from 'styled-components';
 
-import { BulletBarConfigItem, drawBulletBar } from './bullet-bar-helper';
-import classes from './BulletBar.module.scss';
+import { BulletBarDrawer, BulletBarConfigItem, drawBulletBar } from './bullet-bar-helper';
 
 interface BulletBarProps {
   config: BulletBarConfigItem[];
 }
 
+const BulletBarRoot = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  padding: 20px 24px;
+  min-height: 120px;
+  border-radius: 12px;
+  background: #eef5ff;
+
+  .bullet-bar__label-area {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 12px;
+    list-style: none;
+  }
+
+  .bullet-bar__label {
+    position: relative;
+    box-sizing: border-box;
+    flex: 0 0 50%;
+    padding-left: 16px;
+
+
+    font-size: 14px;
+    line-height: 19px;
+    white-space: nowrap;
+  }
+
+  .bullet-bar__decoration {
+    position: absolute;
+    left: 0;
+    top: 50%;
+    width: 8px;
+    height: 8px;
+    border-radius: 100%;
+    transform: translate3d(0, -50%, 0);
+  }
+
+  .bullet-bar__status {
+    display: inline-block;
+    margin-left: 6px;
+  }
+`;
+
 export const BulletBar: FC<BulletBarProps> = ({ config }) => {
   const ref = createRef<HTMLDivElement>();
+  const drawerRef = useRef<BulletBarDrawer | null>(null);
 
   const sortedConfig = useMemo(
     () => config.slice().sort((a: BulletBarConfigItem, b: BulletBarConfigItem): number => a.data - b.data),
@@ -18,31 +63,43 @@ export const BulletBar: FC<BulletBarProps> = ({ config }) => {
   useLayoutEffect(() => {
     if (!ref.current) return;
 
-    const $e = ref.current;
-
-    drawBulletBar($e, config);
-  }, [config, ref]);
+    if (drawerRef.current) {
+      drawerRef.current.update(config);
+    } else {
+      drawerRef.current = new BulletBarDrawer(ref.current);
+      drawerRef.current.create(config);
+      drawerRef.current.update(config);
+    }
+  }, [ref, config]);
 
   return (
-    <div className={classes.root}>
+    <BulletBarRoot>
       <div ref={ref} />
-      <ul className={classes.labelArea}>
+      <ul className='bullet-bar__label-area'>
         {
           sortedConfig.map((item: BulletBarConfigItem): JSX.Element => {
             return (
-              <li className={classes.label}
+              <li
+                className='bullet-bar__label'
                 data-color={item.color}
-                key={`label-${item.label}`}>
-                <div className={classes.decoration}
-                  style={{ background: item.color }} />
+                key={`label-${item.label}`}
+              >
+                <div
+                  className='bullet-bar__decoration'
+                  style={{ background: item.color }}
+                />
                 {item.label}
-                <span className={classes.status}
-                  style={{ color: item.color }}>{item.labelStatus}</span>
+                <span
+                  className='bullet-bar__status'
+                  style={{ color: item.color }}
+                >
+                  {item.labelStatus}
+                </span>
               </li>
             );
           })
         }
       </ul>
-    </div>
+    </BulletBarRoot>
   );
 };
--- packages/ui-components/src/Chart/bullet-bar-helper.ts
+++ packages/ui-components/src/Chart/bullet-bar-helper.ts
@@ -1,8 +1,9 @@
 import * as d3 from 'd3';
+import { debounce } from 'lodash';
 
 type AnySelection = d3.Selection<any, any, any, any>;
 
-type AttrParams = { [x: string]: any }
+type AttrParams = Record<string, any>;
 
 export const mapAttr = (section: AnySelection, attr: AttrParams): AnySelection => {
   Object.keys(attr).forEach((key: string): void => {
@@ -10,10 +11,6 @@ export const mapAttr = (section: AnySelection, attr: AttrParams): AnySelection =
   });
 
   return section;
-};
-
-export const clear = (_d: AnySelection): void => {
-  _d.selectAll('*').remove();
 };
 
 export interface BulletBarConfigItem {
@@ -24,45 +21,59 @@ export interface BulletBarConfigItem {
   dataTransfer?: (data: number) => string;
 }
 
-export const drawBulletBar = ($container: HTMLElement, config: BulletBarConfigItem[]): void => {
-  const _c = d3.select($container);
-  const canvasHeight = 42;
+export class BulletBarDrawer {
+  private $el: HTMLElement;
+  private $container: AnySelection;
+  private $canvas!: AnySelection;
+  private $background!: AnySelection;
+  private canvasHeight: number;
 
-  // clear
-  clear(_c);
+  constructor ($el: HTMLElement) {
+    this.$el = $el;
+    this.$container = d3.select(this.$el);
+    this.canvasHeight = 42;
+  }
 
-  // create d3 canvas
-  const canvas = mapAttr(_c.append('svg'), {
-    fill: 'transparent',
-    height: canvasHeight,
-    width: '100%'
-  });
+  private adjustTextPosition (config: BulletBarConfigItem[]): void {
+    const canvasBox = this.$background.node().getBBox();
 
-  // draw background
-  const background = mapAttr(canvas.append('rect'), {
-    fill: 'transparent',
-    height: '100%',
-    width: '100%'
-  });
+    config.forEach((_item: BulletBarConfigItem, index: number): void => {
+      const current = this.$canvas.select<SVGTextElement>(`#text-${index}`);
+      const next = this.$canvas.select<SVGTextElement>(`#text-${index + 1}`);
+      const currentNode = current.node();
+      const nextNode = next.node();
 
-  // measure canvas size
-  const canvasBox = background.node().getBBox();
+      if (!currentNode) return;
 
-  // initiazlie d3 scale
-  const MAX_NUM = 10 ** 9;
-  const _maxInputNum = Math.min(Math.max.apply(undefined, config.map((item: BulletBarConfigItem): number => item.data)) || 0, MAX_NUM);
+      const currentBox = currentNode.getBBox();
 
-  const scale = d3.scaleLinear().domain([0, _maxInputNum * 1.5]).range([0, canvasBox.width]);
-  const minBarWidth = 6;
+      if (currentBox.x <= 0) {
+        current.attr('x', currentBox.width / 2);
 
-  // draw bar
-  const _sortedConfig = config.slice().sort((a: BulletBarConfigItem, b: BulletBarConfigItem): number => b.data - a.data);
-  const barMaxWidth = canvasBox.width;
-  const barHeight = 6;
-  const barX = 0;
-  const barY = 36;
+        return;
+      }
 
-  const getBarWidth = (data: number): number => {
+      if (canvasBox.width - currentBox.x - currentBox.width / 2 <= 0) {
+        current.attr('x', canvasBox.width - currentBox.width / 2);
+
+        return;
+      }
+
+      // adjust edge
+      if (!nextNode) return;
+      const nextBox = nextNode.getBBox();
+
+      // adjust overlap
+      if (currentBox.x < nextBox.x + nextBox.width) {
+        next.attr('x', currentBox.x - nextBox.width / 2 - 10);
+      }
+    });
+  }
+
+  private getBarWidth (data: number, scale: d3.ScaleLinear<number, number>): number {
+    const canvasBox = this.$background.node().getBBox();
+    const barMaxWidth = canvasBox.width;
+
     if (data === Infinity) {
       return barMaxWidth;
     }
@@ -72,115 +83,176 @@ export const drawBulletBar = ($container: HTMLElement, config: BulletBarConfigIt
     }
 
     return scale(data) || 0;
-  };
+  }
 
-  mapAttr(canvas.append('rect'), {
-    fill: '#ffffff',
-    height: barHeight,
-    rx: barHeight / 2,
-    ry: barHeight / 2,
-    width: barMaxWidth,
-    x: barX,
-    y: barY
-  });
+  public create (config: BulletBarConfigItem[]): void {
+    // create d3 canvas
+    this.$canvas = mapAttr(this.$container.append('svg'), {
+      fill: 'transparent',
+      height: this.canvasHeight,
+      width: '100%'
+    });
 
-  _sortedConfig.forEach((item: BulletBarConfigItem): void => {
-    mapAttr(canvas.append('rect'), {
-      fill: item.color,
+    this.$background = mapAttr(this.$canvas.append('rect'), {
+      fill: 'transparent',
+      height: '100%',
+      width: '100%'
+    });
+
+    const canvasBox = this.$background.node().getBBox();
+    const barHeight = 6;
+    const barX = 0;
+    const barY = 36;
+    const barMaxWidth = canvasBox.width;
+
+    mapAttr(this.$canvas.append('rect'), {
+      class: 'basic-bar',
+      fill: '#ffffff',
       height: barHeight,
       rx: barHeight / 2,
       ry: barHeight / 2,
-      width: getBarWidth(item.data),
+      width: barMaxWidth,
       x: barX,
       y: barY
     });
-    mapAttr(canvas.append('rect'), {
-      fill: item.color,
-      height: barHeight,
-      width: barHeight,
-      x: Math.max(getBarWidth(item.data) - barHeight + barX, 0),
-      y: barY
-    });
-  });
 
-  // draw text
-  _sortedConfig.map((item: BulletBarConfigItem, index: number): AnySelection => {
-    return mapAttr(canvas.append('text'), {
-      fill: item.color,
-      'font-size': 20,
-      'font-weight': 'bold',
-      id: `text-${index}`,
-      'text-anchor': 'middle',
-      x: getBarWidth(item.data) + barX,
-      y: 20
-    }).text(item.dataTransfer ? item.dataTransfer(item.data) : item.data);
-  });
+    const maxInputData = Math.min(
+      Math.max.apply(
+        undefined,
+        config.map((item) => item.data)
+      ),
+      10 ** 9
+    );
 
-  // adjust text position
-  _sortedConfig.forEach((item: BulletBarConfigItem, index: number): void => {
-    const current = canvas.select<SVGTextElement>(`#text-${index}`);
-    const next = canvas.select<SVGTextElement>(`#text-${index + 1}`);
-    const currentNode = current.node();
-    const nextNode = next.node();
+    const scale = d3
+      .scaleLinear()
+      .domain([0, maxInputData * 1.5])
+      .range([0, canvasBox.width]);
 
-    if (!currentNode) return;
+    const _sortedConfig = config
+      .slice()
+      .sort((a: BulletBarConfigItem, b: BulletBarConfigItem): number => b.data - a.data);
 
-    const currentBox = currentNode.getBBox();
+    this.$canvas.selectAll('.bar').data(_sortedConfig)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('fill', (item) => item.color)
+      .attr('height', barHeight)
+      .attr('rx', barHeight / 2)
+      .attr('ry', barHeight / 2)
+      .attr('x', barX)
+      .attr('y', barY)
+      .attr('width', (item) => this.getBarWidth(item.data, scale));
 
-    if (currentBox.x <= 0) {
-      current.attr('x', currentBox.width / 2);
+    this.$canvas.selectAll('.end')
+      .data(_sortedConfig)
+      .enter()
+      .append('rect')
+      .attr('class', 'end')
+      .attr('height', barHeight)
+      .attr('width', barHeight)
+      .attr('fill', (item) => item.color)
+      .attr('x', (item) => Math.max(this.getBarWidth(item.data, scale) - barHeight + barX, 0))
+      .attr('y', barY);
 
-      return;
-    }
+    this.$canvas.selectAll('.label')
+      .data(_sortedConfig)
+      .enter()
+      .append('text')
+      .attr('class', 'label');
 
-    if (canvasBox.width - currentBox.x - currentBox.width / 2 <= 0) {
-      current.attr('x', canvasBox.width - currentBox.width / 2);
+    this.$canvas.selectAll('.poly-line')
+      .remove()
+      .exit()
+      .data(_sortedConfig)
+      .enter()
+      .append('polyline')
+      .attr('class', 'poly-line');
+  }
 
-      return;
-    }
+  public update = debounce((config: BulletBarConfigItem[]): void => {
+    const canvasBox = this.$background.node().getBBox();
+    const maxInputData = Math.min(
+      Math.max.apply(
+        undefined,
+        config.map((item) => item.data)
+      ),
+      10 ** 9
+    );
+    const barHeight = 6;
+    const barX = 0;
+    const barY = 36;
+    const minBarWidth = 6;
 
-    // adjust edge
+    const scale = d3
+      .scaleLinear()
+      .domain([0, maxInputData * 1.5])
+      .range([0, canvasBox.width]);
 
-    if (!nextNode) return;
-    const nextBox = nextNode.getBBox();
+    const _sortedConfig = config
+      .slice()
+      .sort((a: BulletBarConfigItem, b: BulletBarConfigItem): number => b.data - a.data);
 
-    // adjust overlap
-    if (currentBox.x < (nextBox.x + nextBox.width)) {
-      next.attr('x', currentBox.x - nextBox.width / 2 - 10);
-    }
-  });
+    this.$canvas.selectAll('.label')
+      .data(_sortedConfig)
+      .attr('id', (_item, index) => `text-${index}`)
+      .attr('fill', (item) => item.color)
+      .attr('font-size', 20)
+      .attr('font-weight', 'bold')
+      .attr('text-anchor', 'middle')
+      .attr('y', 20)
+      .text((item) => item.dataTransfer ? item.dataTransfer(item.data) : item.data)
+      .attr('x', (item) => this.getBarWidth(item.data, scale) + barX);
 
-  // draw polyline
-  _sortedConfig.forEach((item: BulletBarConfigItem, index: number): void => {
-    const text = canvas.select<SVGTextElement>(`#text-${index}`);
-    const textNode = text.node();
+    this.adjustTextPosition(_sortedConfig);
 
-    if (!textNode) return;
+    this.$canvas.selectAll('.poly-line')
+      .data(_sortedConfig)
+      .attr('fill', 'none')
+      .attr('points', (item, index): any => {
+        const text = this.$canvas.select<SVGTextElement>(`#text-${index}`);
+        const textNode = text.node();
 
-    const textBox = textNode.getBBox();
-    const barX2 = barX + getBarWidth(item.data);
-    const pointAtBarEndX = Math.max(barX2 - 0.5, minBarWidth);
-    let points = [];
+        if (!textNode) return [];
 
-    if (index === 0 || textBox.x + textBox.width - barX2 > 0) {
-      points = [
-        pointAtBarEndX, barY - barHeight, // point 3
-        pointAtBarEndX, barY // point 4
-      ];
-    } else {
-      points = [
-        textBox.x, textBox.y + textBox.height, // point 1
-        textBox.x + textBox.width, textBox.y + textBox.height, // point 2
-        pointAtBarEndX, barY - barHeight, // point 3
-        pointAtBarEndX, barY // point 4
-      ];
-    }
+        const textBox = textNode.getBBox();
+        const barX2 = barX + this.getBarWidth(item.data, scale);
+        const pointAtBarEndX = Math.max(barX2 - 0.5, minBarWidth);
 
-    mapAttr(canvas.append('polyline'), {
-      fill: 'none',
-      points: points,
-      'stoke-width': 1,
-      stroke: item.color
-    });
-  });
-};
+        if (index === 0 || textBox.x + textBox.width - barX2 > 0) {
+          return [
+            pointAtBarEndX,
+            barY - barHeight, // point 3
+            pointAtBarEndX,
+            barY // point 4
+          ];
+        } else {
+          return [
+            textBox.x,
+            textBox.y + textBox.height, // point 1
+            textBox.x + textBox.width,
+            textBox.y + textBox.height, // point 2
+            pointAtBarEndX,
+            barY - barHeight, // point 3
+            pointAtBarEndX,
+            barY // point 4
+          ];
+        }
+      })
+      .attr('stroke-width', 1)
+      .attr('stroke', (item) => item.color);
+
+    this.$canvas.selectAll('.bar').data(_sortedConfig)
+      .attr('fill', (item) => item.color)
+      .transition()
+      .duration(400)
+      .attr('width', (item) => this.getBarWidth(item.data, scale));
+
+    this.$canvas.selectAll('.end').data(_sortedConfig)
+      .attr('fill', (item) => item.color)
+      .transition()
+      .duration(400)
+      .attr('x', (item) => Math.max(this.getBarWidth(item.data, scale) - barHeight + barX, 0));
+  }, 300)
+}
